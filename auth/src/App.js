04478c7f080041646c5049dd1f0f900e1560fd89
Kyungbin Ryu <rkbin@hayanmind.com>
2019-01-04 22:12:58 +0900

    Processing Authentication Credentials
    use firebase login authentication system

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
     componentWillMount() {
         firebase.initializeApp({
             // API KEY
           });
-    }
-    render() {
-        return (
+          firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+              this.setState({ loggedIn: true });
+            } else {
+              this.setState({ loggedIn: false });
+            }
+          });
+        }
+      
+        renderContent() {
+          switch (this.state.loggedIn) {
+            case true:
+              return (
+                  <CardSection>
+                    <Button onPress={() => firebase.auth().signOut()}>
+                        Log Out
+                    </Button>
+                  </CardSection>
+              );
+            case false:
+              return <LoginForm />;
+            default:
+              return <Spinner size="large" />;
+          }
+        }
+      
+        render() {
+          return (
             <View>
-                <Header headerText="Authentication"></Header>
-                <LoginForm />
+              <Header headerText="Authentication" />
+              {this.renderContent()}
             </View>
-        );
-    }
-}
-export default App;
+          );
+        }
+      }
+      
+      export default App;
