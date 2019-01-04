335d5fb3e16ef03d95fcbc306f5472d63af5323a
Kyungbin Ryu <rkbin@hayanmind.com>
2019-01-04 16:06:41 +0900

    Auth page practice
    make login form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDGvgmlb5GvX5gXuYoW8dTKG2YCSfhzNFw",
+            authDomain: "auth-b3b1d.firebaseapp.com",
+            databaseURL: "https://auth-b3b1d.firebaseio.com",
+            projectId: "auth-b3b1d",
+            storageBucket: "auth-b3b1d.appspot.com",
+            messagingSenderId: "221767404302"
+          });
+    }
     render() {
         return (
             <View>
-                <Text>An app</Text>
+                <Header headerText="Authentication"></Header>
+                <LoginForm />
             </View>
         );
     }
 }
-
 export default App;
